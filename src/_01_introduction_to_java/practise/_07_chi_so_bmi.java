aa7e5b3dfe197b3f9e4a10937b88c526535ac6d2
kaioshyn <nguyenduc251196@gmail.com>
2021-06-02 17:16:59 +0700

    commit_bai_tap_1

---
+++ src/_01_introduction_to_java/practise/_07_chi_so_bmi.java
@@ -0,0 +1,27 @@
+package _01_introduction_to_java.practise;
+
+import java.util.Scanner;
+
+public class _07_chi_so_bmi {
+    public static void main(String[] args) {
+        double weight, height, bmi;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Your weight (in kilogram):");
+        weight = scanner.nextDouble();
+
+        System.out.print("Your height (vd: 1,65 in meter) :");
+        height = scanner.nextDouble();
+
+        bmi = weight / Math.pow(height, 2);
+        System.out.println(bmi);
+        if (bmi < 18)
+            System.out.println(bmi + " Underweight");
+        else if (bmi < 25.0)
+            System.out.println(bmi + " Normal");
+        else if (bmi < 30.0)
+            System.out.println(bmi + " Overweight");
+        else
+            System.out.println(bmi + " Obese");
+    }
+}
