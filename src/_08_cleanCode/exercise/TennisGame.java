3db350663043a6d4c9731d04c9395ddcc1a3ad14
kaioshyn <nguyenduc251196@gmail.com>
2021-06-16 11:32:26 +0700

    fix_clean_bai8

--- src/_08_cleanCode/exercise/TennisGame.java
+++ src/_08_cleanCode/exercise/TennisGame.java
@@ -2,57 +2,73 @@ package _08_cleanCode.exercise;
 
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
-        StringBuilder score = new StringBuilder();
-        int tempScore = 0;
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
-                case 0:
-                    score = new StringBuilder("Love-All");
-                    break;
-                case 1:
-                    score = new StringBuilder("Fifteen-All");
-                    break;
-                case 2:
-                    score = new StringBuilder("Thirty-All");
-                    break;
-                case 3:
-                    score = new StringBuilder("Forty-All");
-                    break;
-                default:
-                    score = new StringBuilder("Deuce");
-                    break;
+    public static StringBuilder score = new StringBuilder();
 
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = new StringBuilder("Advantage player1");
-            else if (minusResult == -1) score = new StringBuilder("Advantage player2");
-            else if (minusResult >= 2) score = new StringBuilder("Win for player1");
-            else score = new StringBuilder("Win for player2");
+    public static String getScore(String playerNameOne, String playerNameTwo, int scoreOne, int scoreTwo) {
+        score = new StringBuilder();
+        if (scoreOne == scoreTwo) {
+            equalPoint(scoreTwo);
+        } else if (scoreOne >= 4 || scoreTwo >= 4) {
+            finalPoint(scoreOne, scoreTwo);
         } else {
-            for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
-                    score.append("-");
-                    tempScore = m_score2;
-                }
-                switch (tempScore) {
-                    case 0:
-                        score.append("Love");
-                        break;
-                    case 1:
-                        score.append("Fifteen");
-                        break;
-                    case 2:
-                        score.append("Thirty");
-                        break;
-                    case 3:
-                        score.append("Forty");
-                        break;
-                }
-            }
+            pointInMatch(scoreOne, scoreTwo);
         }
         return score.toString();
+    }
+
+    public static void equalPoint(int point) {
+        switch (point) {
+            case 0:
+                score = new StringBuilder("Love-All");
+                break;
+            case 1:
+                score = new StringBuilder("Fifteen-All");
+                break;
+            case 2:
+                score = new StringBuilder("Thirty-All");
+                break;
+            case 3:
+                score = new StringBuilder("Forty-All");
+                break;
+            default:
+                score = new StringBuilder("Deuce");
+                break;
+        }
+    }
+
+    public static void finalPoint(int pointOne, int pointTwo) {
+        int minusResult = pointOne - pointTwo;
+        if (minusResult == 1) score = new StringBuilder("Advantage player1");
+        else if (minusResult == -1) score = new StringBuilder("Advantage player2");
+        else if (minusResult >= 2) score = new StringBuilder("Win for player1");
+        else score = new StringBuilder("Win for player2");
+    }
+
+    public static void pointInMatch(int pointOne, int pointTwo) {
+        int tempPoint = 0;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                tempPoint = pointOne;
+            }
+            else {
+                score.append("-");
+                tempPoint = pointTwo;
+            }
+            switch (tempPoint) {
+                case 0:
+                    score.append("Love");
+                    break;
+                case 1:
+                    score.append("Fifteen");
+                    break;
+                case 2:
+                    score.append("Thirty");
+                    break;
+                case 3:
+                    score.append("Forty");
+                    break;
+            }
+        }
+
     }
 }
--- src/test/test1.java
+++ src/test/test1.java
@@ -1,12 +1,14 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class test1 {
-    public static void main(String[] args){
-        int[] intarr = new int[]{4, 5, 6};
-        for(int i = 0; i < intarr.length; i++)
-            System.out.print(intarr[i]);
-    }
+//    public static void main(String[] args){
+//        System.out.print("A");
+//    }
+//    public static void main(){
+//        System.out.print("B");
+//    }
+//    public static void start(String[] args){
+//        System.out.print("C");
+//    }
+   
 }
